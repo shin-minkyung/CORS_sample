b74410b1313538c08bcbb3bea4b504a612651b9f
Shin Min Kyung <haenemm@naver.com>
2019-04-22 18:38:27 +0900

    able to update yml in GIT

--- configServer/src/main/java/com/config/WebController.java
+++ configServer/src/main/java/com/config/WebController.java
@@ -4,12 +4,17 @@ package com.config;
 import static org.assertj.core.api.Assertions.in;
 import static org.hamcrest.CoreMatchers.instanceOf;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.codec.binary.Base64;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.omg.CORBA.OBJ_ADAPTER;
 import org.springframework.cloud.bus.event.AckRemoteApplicationEvent;
 import org.springframework.cloud.bus.event.RefreshListener;
 import org.springframework.cloud.bus.event.RefreshRemoteApplicationEvent;
@@ -20,15 +25,23 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.event.EventListener;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.yaml.snakeyaml.Yaml;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+import com.google.gson.internal.LinkedHashTreeMap;
 
 @RestController
 public class WebController {
@@ -49,6 +62,7 @@ public class WebController {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 		
+		
 		LinkedMultiValueMap<String, String> param =  new LinkedMultiValueMap<String, String>();	  
 		param.add("path", "*");
 		
@@ -60,8 +74,8 @@ public class WebController {
 	  }
 	  
 	  
-	  @GetMapping("/getConfig")
-	  public String getConfig(String username, String repo, String rootPath){
+	  @RequestMapping("/getConfig")	  
+	  public String getConfig(String username, String repo, String rootPath, LinkedHashMap<String, Object> Iterator) throws IOException{
 		  //file name is not included in "rootPath"
 		  String url = "https://api.github.com/repos/"+username+"/"+repo+"/contents"+rootPath;
 		  
@@ -163,9 +177,41 @@ public class WebController {
 		  
 		  
 		  //conversion result
-		  System.err.println(" Obj from Yaml "+obj);
+		  System.err.println("result obj"+obj);
+		  System.err.println("result map"+map);
 		  
-		  return new String(decodedContent);
+		  Yaml newYaml = new Yaml();
+		  ObjectMapper mapper = new ObjectMapper();
+		  String objJSON = mapper.writeValueAsString(obj);
+		  JsonNode jsonNodeTree = new ObjectMapper().readTree(objJSON);
+		  String newYmlString = new YAMLMapper().writeValueAsString(jsonNodeTree);
+		  
+		  //String newYmlString =  newYaml.dumpAll((java.util.Iterator<? extends Object>) obj);
+		  String encodedString = Base64.encodeBase64String(newYmlString.getBytes());
+		  System.err.println("newYmlString "+newYmlString);
+		  System.err.println("encodedString "+encodedString);
+  
+	  HttpHeaders headers = new HttpHeaders();
+	  headers.setContentType(MediaType.APPLICATION_JSON);
+	  headers.set("Authorization", "token 146683bafa4f32ddd7eb56940526e5c84624d7c4");
+	  
+	  
+//	  JSONObject putJson = new JSONObject();
+//	  putJson.put("message","ffff" );
+//	  putJson.put("content", encodedString );		  
+	  String sha = (String) fileJson.getBody().get("sha");
+//	  putJson.put("sha", sha );
+	  
+	  String param = "{\"message\":\"this is a message\",";
+	  param += "\"content\":\""+encodedString+"\", ";
+	  param += "\"sha\": \""+sha+"\"}";
+	  
+	  System.err.println("param "+param);
+	
+		 // HttpEntity<String> request = new HttpEntity<>(param, headers);		 
+		 // ResponseEntity<String> final_response= restTemplate.exchange(file_url, HttpMethod.PUT, request, String.class);
+		  
+		  return newYmlString;
 	  }
 	  
 	  
